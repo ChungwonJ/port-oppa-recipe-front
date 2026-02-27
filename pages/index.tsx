99422f0521c435a655a00437e97af6f098462ce2
Chungwon Jung <wjdcjddnjs1234@gmail.com>
2026-02-28 01:31:13 +0900

    ㅊㅊㅊ

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>PortForU - 포트폴리오</title>
-        <meta name="description" content="PortForU-포트폴리오" />
+        <title>오빠레시피 - 포트폴리오</title>
+        <meta name="description" content="오빠레시피-포트폴리오" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
